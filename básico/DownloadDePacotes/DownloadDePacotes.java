a330520fa1e9f4a9c1ae63f33ae4266aa2866214
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-03-15 11:58:26 -0400

    Challenge: Download Packages

--- básico/DownloadDePacotes/DownloadDePacotes.java
+++ básico/DownloadDePacotes/DownloadDePacotes.java
@@ -1,3 +1,4 @@
+package básico.DownloadDePacotes;
 /*
  * Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:  
  * - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
@@ -6,23 +7,21 @@
 
 import java.util.Scanner;
 
-public class MesadaDoSobrinho {
+public class DownloadDePacotes {
   public static void main(String[] args) {
     Scanner leitor = new Scanner(System.in);
 
-    System.out.print("Insira uma entrada: ");
-    int entrada = leitor.nextInt();
-
-    int mesada = 50;
+    System.out.print("Insira um tamanho: ");
+    int tamanho = leitor.nextInt();
 
     /*
      * TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro
      * irá juntar nos meses da entrada
      */
 
-    int total = mesada * entrada;
-
-    System.out.println("O sobrinho juntou " + total + "$");
+    for (int i = 0; i < tamanho; i++) {
+      System.out.print("/");
+    }
 
   }
 }
